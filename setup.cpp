495f554a3fbfe374779beb9950cb6b6fc1eb6094
sp3ktful <106328886+viktorsynek@users.noreply.github.com>
2022-10-14 19:17:44 +0200

    main file

---
+++ setup.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void test(){
+	// Happy Coding 🌿
+}
+
+int main()
+{
+    int t = 1;
+    cin >> t;
+    while(t--){
+    	test();
+    }
+    
+    return 0;
+}
